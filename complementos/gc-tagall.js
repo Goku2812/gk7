2134bfc2ccfc7d66be4dcb1e735b03641f42197a
Goku2812 <115955385+Goku2812@users.noreply.github.com>
2022-12-20 09:57:26 -0500

    Update and rename plugins/gc-tagall.js to complementos/gc-tagall.js

--- complementos/gc-tagall.js
+++ complementos/gc-tagall.js
@@ -7,7 +7,7 @@ let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
 let teks = `𝐀𝐍𝐆𝐄𝐋𝐄𝐒 𝐒𝐔𝐏𝐄𝐑𝐈𝐎𝐑𝐄𝐒\n\n ${oi}\n\n\n`
 for (let mem of participants) {
-teks += `┣ @${mem.id.split('@')[0]}\n`}
+teks += ` 🎅@${mem.id.split('@')[0]}\n`}
 teks += `*└* 𝙶𝙾𝙺𝚄 𝙱𝙾𝚃\n\n*𝙂𝙤𝙠𝙪𝘽𝙤𝙩*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
